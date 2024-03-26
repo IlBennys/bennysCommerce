dbb766afd3dd925db153b7ab3b17c1215502d079
Luca Scomazzon <scom.luca@gmail.com>
2024-03-26 17:51:51 +0100

    Fix security and finished

--- bennys-commerce/src/main/java/com/bennyscommerce/auth/configuration/SecurityConfig.java
+++ bennys-commerce/src/main/java/com/bennyscommerce/auth/configuration/SecurityConfig.java
@@ -1,10 +1,70 @@
 package com.bennyscommerce.auth.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.bennyscommerce.auth.security.JwtAuthenticationEntryPoint;
+import com.bennyscommerce.auth.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @SuppressWarnings("unused")
+    private UserDetailsService userDetailsService;
+
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+
+    private JwtAuthenticationFilter authenticationFilter;
+
+    public SecurityConfig(UserDetailsService userDetailsService, JwtAuthenticationEntryPoint authenticationEntryPoint,
+	    JwtAuthenticationFilter authenticationFilter) {
+	this.userDetailsService = userDetailsService;
+	this.authenticationEntryPoint = authenticationEntryPoint;
+	this.authenticationFilter = authenticationFilter;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+	return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+	return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	http.cors().and().csrf().disable()
+		.authorizeHttpRequests((authorize) -> authorize.requestMatchers(HttpMethod.POST, "/api/auth/**")
+			.permitAll().requestMatchers(HttpMethod.GET, "/api/articolo").permitAll()
+			.requestMatchers(HttpMethod.GET, "/api/carrello", "/api/ordine", "/api/user")
+			.hasAnyRole("USER", "ADMIN").requestMatchers(HttpMethod.POST, "/api/carrello", "/api/ordine")
+			.hasAnyRole("USER", "ADMIN").requestMatchers(HttpMethod.PUT, "/api/ordine", "/api/user")
+			.hasAnyRole("USER", "ADMIN").requestMatchers(HttpMethod.DELETE, "/api/carrello", "/api/user")
+			.hasAnyRole("USER", "ADMIN").requestMatchers(HttpMethod.POST, "/api/articolo").hasRole("ADMIN")
+			.requestMatchers(HttpMethod.PUT, "/api/articolo").hasRole("ADMIN")
+			.requestMatchers(HttpMethod.DELETE, "/api/articolo", "/api/ordine").hasRole("ADMIN")
+			.anyRequest().authenticated())
+		.exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint))
+		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+	http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+	return http.build();
+
+    }
 
 }
---
+++ bennys-commerce/src/main/java/com/bennyscommerce/auth/controller/AuthController.java
@@ -0,0 +1,43 @@
+package com.bennyscommerce.auth.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bennyscommerce.auth.payload.JWTAuthResponse;
+import com.bennyscommerce.auth.payload.LoginDto;
+import com.bennyscommerce.auth.payload.RegisterDto;
+import com.bennyscommerce.auth.service.AuthService;
+
+import lombok.AllArgsConstructor;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private AuthService authService;
+
+    @PostMapping(value = { "/login", "/signin" })
+    public ResponseEntity<JWTAuthResponse> login(@RequestBody LoginDto loginDto) {
+
+	String token = authService.login(loginDto);
+
+	JWTAuthResponse jwtAuthResponse = new JWTAuthResponse();
+	jwtAuthResponse.setUsername(loginDto.getUsername());
+	jwtAuthResponse.setAccessToken(token);
+
+	return ResponseEntity.ok(jwtAuthResponse);
+    }
+
+    @PostMapping(value = { "/register", "/signup" })
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+	String response = authService.register(registerDto);
+	return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
