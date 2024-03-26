ef0fdcb6836feccb4def36678722174086b527e8
Luca Scomazzon <scom.luca@gmail.com>
2024-03-26 20:04:47 +0100

    Fix comments in the exception files

--- bennys-commerce/src/main/java/com/bennyscommerce/auth/exception/GlobalExceptionHandler.java
+++ bennys-commerce/src/main/java/com/bennyscommerce/auth/exception/GlobalExceptionHandler.java
@@ -21,7 +21,6 @@ import com.bennyscommerce.auth.payload.ErrorDetails;
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    // handle specific exceptions
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
 	    WebRequest webRequest) {
@@ -37,7 +36,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
-    // global exceptions
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest) {
 	ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
@@ -57,18 +55,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
-
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception,
-//                                                                        WebRequest webRequest){
-//        Map<String, String> errors = new HashMap<>();
-//        exception.getBindingResult().getAllErrors().forEach((error) ->{
-//            String fieldName = ((FieldError)error).getField();
-//            String message = error.getDefaultMessage();
-//            errors.put(fieldName, message);
-//        });
-//        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-//    }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorDetails> handleAccessDeniedException(AccessDeniedException exception,
--- bennys-commerce/src/main/java/com/bennyscommerce/auth/repository/RoleRepository.java
+++ bennys-commerce/src/main/java/com/bennyscommerce/auth/repository/RoleRepository.java
@@ -8,5 +8,6 @@ import com.bennyscommerce.auth.entity.Erole;
 import com.bennyscommerce.auth.entity.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
     Optional<Role> findByErole(Erole erole);
 }
