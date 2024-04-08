b8d67699554ac80843b64602ea6555b4fbcb1cec
IlBennys <benedettomanfre99@gmail.com>
2024-04-08 19:08:30 +0200

    fix folder

--- client/src/componets/PageNotFound.jsx
+++
@@ -1,7 +0,0 @@
-import "../assets/sass/PageNotFound.scss";
-
-const PageNotFound = () => {
-  return <>ERROR 404</>;
-};
-
-export default PageNotFound;
--- client/src/componets/Profilo.jsx
+++
@@ -1,5 +0,0 @@
-const Profilo = () => {
-  return <>Profilo</>;
-};
-
-export default Profilo;
